4b51cc309f7a7ad12d956c31d7227cfaffb069d9
Ladislas de Toldi <ladislas@leka.io>
2019-04-24 10:04:08 +0200

    hello-world 2.0.0
    Closes #6.
    
    Signed-off-by: Ladislas de Toldi <ladislas@leka.io>

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -2,8 +2,8 @@ class HelloWorld < Formula
   desc "Simple C++ Hello World CLI to play with Homebrew"
   homepage "https://github.com/ladislas/hello-world"
 
-  url "https://github.com/ladislas/hello-world/archive/1.0.0.tar.gz"
-  sha256 "1bdbafc6e059eca4421c2077c598cdf08a73f7115a4b6301ba612767260b5348"
+  url "https://github.com/ladislas/hello-world/archive/2.0.0.tar.gz"
+  sha256 "73b144b542e5abd12a99c33acd38e415b6e8f4bc91d6d4fe3efe705b7abc681a"
 
   def install
     system "make"
