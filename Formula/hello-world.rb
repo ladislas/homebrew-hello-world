2cd07dd24868b8727f83786e292741f36dd2f327
Ladislas de Toldi <ladislas@leka.io>
2019-04-24 13:53:11 +0200

    hello-world 3.0.0

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -2,8 +2,8 @@ class HelloWorld < Formula
   desc "Simple C++ Hello World CLI to play with Homebrew"
   homepage "https://github.com/ladislas/hello-world"
 
-  url "https://github.com/ladislas/hello-world/archive/2.0.0.tar.gz"
-  sha256 "73b144b542e5abd12a99c33acd38e415b6e8f4bc91d6d4fe3efe705b7abc681a"
+  url "https://github.com/ladislas/hello-world/archive/3.0.0.tar.gz"
+  sha256 "086d27aa69124b73859622c58f4f96a29214cbd4fed8ac8f9f845789b178903e"
 
   bottle do
     root_url "https://dl.bintray.com/ladislas/bottles-hello-world"
