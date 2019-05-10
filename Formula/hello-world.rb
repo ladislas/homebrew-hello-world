54db0a966e232609e63006450c44322c106cc960
ladislas <ladislas@detoldi.me>
2019-05-10 17:33:34 +0000

    hello-world: update 7.0.0 bottle.

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -8,8 +8,8 @@ class HelloWorld < Formula
   bottle do
     root_url "https://dl.bintray.com/ladislas/bottles-hello-world"
     cellar :any_skip_relocation
-    sha256 "116be103f2c306693d001da2c3a51ff5676c205120f06e5e0acf9ed3d3994afd" => :mojave
-    sha256 "472952da612e0d2bdd4dac222f4a61f20228d59e4397c8e9fada3f1162ba3e42" => :high_sierra
+    sha256 "04e98bdd4e6cb409f6c1ec450983abca0e8a67fcc040692b424bf51d773c6cb4" => :mojave
+    sha256 "c12f3353db4871492fc587a6a4932fce1ed0bcb4e8d11315439f85afb77115a7" => :high_sierra
   end
 
   def install
