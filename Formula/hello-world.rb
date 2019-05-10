c234cf3e835a63d928f37a0e840389f931cb969b
Ladislas de Toldi <ladislas@leka.io>
2019-05-10 16:45:12 +0200

    hello-world 7.0.0
    Closes #12.
    
    Signed-off-by: Ladislas de Toldi <ladislas@leka.io>

--- Formula/hello-world.rb
+++ Formula/hello-world.rb
@@ -2,8 +2,8 @@ class HelloWorld < Formula
   desc "Simple C++ Hello World CLI to play with Homebrew"
   homepage "https://github.com/ladislas/hello-world"
 
-  url "https://github.com/ladislas/hello-world/archive/6.0.0.tar.gz"
-  sha256 "d79343fe8bc1e2a9870ffba4ab2c2108a6e3cf75ad9a989eb56baffc6d8d215f"
+  url "https://github.com/ladislas/hello-world/archive/7.0.0.tar.gz"
+  sha256 "c1bc5eb9621fd629cc6972dafe08cc9f345cd4f8755108ed5389b77024222078"
 
   bottle do
     root_url "https://dl.bintray.com/ladislas/bottles-hello-world"
